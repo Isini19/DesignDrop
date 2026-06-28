7996a13c67425039307815b4286e43b2e90b21dd
Isini19 <isiniudarahm@gmail.com>
2026-06-28 19:58:20 +0530

    feat:scaffold springboot backend

---
+++ backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/DesigndropBackendApplication.java
@@ -0,0 +1,13 @@
+package com.designdrop.designdropbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesigndropBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesigndropBackendApplication.class, args);
+    }
+
+}
---
+++ backend/designdrop-backend/src/test/java/com/designdrop/designdropbackend/DesigndropBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designdrop.designdropbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesigndropBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
