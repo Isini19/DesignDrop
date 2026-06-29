b3accccac3134a5b55652803d9c4d5a6fbfce233
Isini19 <isiniudarahm@gmail.com>
2026-06-29 23:44:50 +0530

    feat:add CartItem entity

--- backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/CartItem.java
+++ backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/CartItem.java
@@ -1,4 +1,25 @@
 package com.designdrop.designdropbackend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cart_items")
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "listing_id", nullable = false)
+    private Listing listing;
+
+    @Column(nullable = false)
+    private Integer quantity = 1;
 }
