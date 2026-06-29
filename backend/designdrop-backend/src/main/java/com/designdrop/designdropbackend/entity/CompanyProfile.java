0756c45df0e0bd60cdb1c608aff2be4c261e98e8
Isini19 <isiniudarahm@gmail.com>
2026-06-29 23:27:11 +0530

    feat:add CompanyProfile entity

--- backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/CompanyProfile.java
+++ backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/CompanyProfile.java
@@ -1,4 +1,24 @@
 package com.designdrop.designdropbackend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "company_profiles")
 public class CompanyProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long companyId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String companyName;
+
+    private String description;
+    private String logo;
 }
