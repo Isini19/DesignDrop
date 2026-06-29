f410cdb211bb2b01e18272edf94a698545b68920
Isini19 <isiniudarahm@gmail.com>
2026-06-29 23:31:37 +0530

    feat:add Category entity

--- backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/Category.java
+++ backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/Category.java
@@ -1,4 +1,17 @@
 package com.designdrop.designdropbackend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
 }
