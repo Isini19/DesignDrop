83864fd0ecafaf0b4a0c20089ff2a4fd00f0be34
Isini19 <isiniudarahm@gmail.com>
2026-06-29 23:19:23 +0530

    feat:add Designer Profile entity

--- backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/DesignerProfile.java
+++ backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/entity/DesignerProfile.java
@@ -1,4 +1,22 @@
 package com.designdrop.designdropbackend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "designer_profiles")
 public class DesignerProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long profileId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    private String displayName;
+    private String bio;
+    private String profilePicture;
 }
