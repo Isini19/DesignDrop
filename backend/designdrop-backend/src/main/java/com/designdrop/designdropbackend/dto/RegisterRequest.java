c81a6b4437aac5532aab1214d474d5e4b7dfcb00
Isini19 <isiniudarahm@gmail.com>
2026-07-01 11:30:52 +0530

    feat:add Register Request DTO class

--- backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/dto/RegisterRequest.java
+++ backend/designdrop-backend/src/main/java/com/designdrop/designdropbackend/dto/RegisterRequest.java
@@ -1,4 +1,15 @@
 package com.designdrop.designdropbackend.dto;
 
+
+import com.designdrop.designdropbackend.entity.User;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String email;
+    private String password;
+    private User.Role role;
 }
+
+
+//user signup
